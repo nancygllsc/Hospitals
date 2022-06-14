a635cd9338c21f8ac637c22a0b0923fcc6ea0323
Nancy Pulido <nancypulido@Nancys.local>
2022-06-13 20:42:51 -0700

    FILES

--- formulas.R
+++ formulas.R
@@ -1,1 +1,11 @@
 #formulas ... test 
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+#see head 
+head(outcome)
+#see see how many columns
+ncol(outcome)
+#see the names of each column
+names(outcome)
+#simple histogram of the 30-day death rates from heart attack (column 11 in the outcome dataset)
+outcome[, 11] <- as.numeric(outcome[, 11])
+hist(outcome[, 11])
